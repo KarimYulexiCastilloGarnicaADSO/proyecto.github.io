b454e2a3f00d26cb10c71219341b1ff52da3cb95
karim castillo <karimcastillogarnica@gmail.com>
2024-07-22 21:05:14 -0500

    c

--- Js/app.js
+++ Js/app.js
@@ -177,13 +177,8 @@ const capturar = (event) =>{
     enviar(datos)
 }
 
-// const tomar = (event) => {
-//     event.preventDefault()
-//     const dt = {
-//         usuario: usuario.value,
-//         contraseña: contraseña.value
-//     }
-// }
+
+
 /**
  * Envia los datos
  */
@@ -201,17 +196,3 @@ async function enviar(datos) {
 
 $form.addEventListener("submit" , capturar)
 
-
-// async function registrar(dt) {
-//     fetch('http://127.0.0.1:3000/Usuarios', {
-//         method: 'POST',
-//         body: JSON.stringify(dt),
-//         headers: {
-//           'Content-type': 'application/json; charset=UTF-8',
-//         },
-//       })
-//         .then((response) => response.json())
-//         .then((json) => console.log(json));
-// }
-
-// formu_usu.addEventListener("submit" , tomar)
